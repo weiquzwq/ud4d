445885521ea982a7c7b5b327de9f1460cb4324d7
williamfzc <178894043@qq.com>
2019-01-28 05:17:27 -0800

    release: 0.1.0

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='ud4d',
+    version='0.1.0',
+    description='USB Device Detector for Docker usage. Support Linux only.',
+    author='williamfzc',
+    author_email='fengzc@vip.qq.com',
+    url='https://github.com/doringland/ud4d',
+    packages=find_packages(),
+    install_requires=[
+        'logzero',
+        'docker',
+    ]
+)
