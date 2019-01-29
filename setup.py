9e7c0bd3856597650be14b354c828444af89c83c
williamfzc <178894043@qq.com>
2019-01-29 15:22:31 +0800

    release: 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ud4d',
-    version='0.1.0',
+    version='0.1.1',
     description='USB Device Detector for Docker usage. Support Linux only.',
     author='williamfzc',
     author_email='fengzc@vip.qq.com',
